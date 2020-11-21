2efa501985aeff5c5b10b926edca58c175e33c69
lcy <1395170018@qq.com>
2020-11-22 00:44:50 +0800

    2020年11月22日00:44:48

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,81 @@
 <template>
-  <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
+  <div class="detail">
+    <detail-nav-bar
+      @titleClick="titleClick"
+      class="detail-nav"
+      ref="nav"
+    ></detail-nav-bar>
+    <scroll
+      class="content"
+      ref="scroll"
+      :probe-type="3"
+      @scroll="contentScroll"
+    >
+      <ul>
+        <li v-for="item in $store.state.cartList">{{ item }}</li>
+      </ul>
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods" />
+      <detail-shop-info :shop="shop" />
+      <detail-goods-info :detail-info="detailInfo" />
+      <detail-param-info ref="params" :param-info="paramInfo" />
+      <detail-comment-info ref="comment" :comment-info="commentInfo" />
+      <goods-list ref="recommend" :goods="recommends" />
+    </scroll>
+    <detail-bottom-bar @addCart="addToCart" />
+    <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
   </div>
 </template>
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from "./childComps/DetailSwiper";
-import { getDetail } from "network/detail";
+import DetailBaseInfo from "./childComps/DetailBaseInfo";
+import DetailShopInfo from "./childComps/DetailShopInfo";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+
+import DetailParamInfo from "./childComps/DetailParamInfo";
+import DetailCommentInfo from "./childComps/DetailCommentInfo";
+import DetailBottomBar from "./childComps/DetailBottomBar";
+
+import Scroll from "components/common/scroll/Scroll";
+import GoodsList from "components/content/goods/GoodsList";
+import BackTop from "components/content/backTop/BackTop";
+
+import {
+  getDetail,
+  Goods,
+  Shop,
+  GoodsParam,
+  getRecommend,
+} from "network/detail";
 export default {
   components: {
     DetailNavBar,
     DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo,
+    GoodsList,
+    DetailBottomBar,
+    BackTop,
   },
   data() {
     return {
       iid: null,
       topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {},
+      recommends: [],
+      themeTopYs: [],
+      getThemeTop: null,
+      currentIndex: 0,
+      isShowBackTop: false,
     };
   },
   created() {
@@ -27,11 +86,141 @@ export default {
     getDetail(this.iid).then((res) => {
       // console.log(res);
       //1.获取顶部的图片轮播数据
-      console.log(res);
-      this.topImages = res.result.itemInfo.topImages;
+      // console.log(res);
+      const data = res.result;
+      this.topImages = data.itemInfo.topImages;
+
+      //2.获取商品信息
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+
+      //3.创建店铺信息的对象
+      this.shop = new Shop(data.shopInfo);
+
+      //4.保存商品的详情数据
+      this.detailInfo = data.detailInfo;
+
+      //5.获取参数的信息
+      this.paramInfo = new GoodsParam(
+        data.itemParams.info,
+        data.itemParams.rule
+      );
+      //6.取出评论的信息
+      if (data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list[0];
+      }
     });
+
+    // this.$nextTick(() => {
+    //   this.themeTopYs = [];
+
+    //   this.themeTopYs.push(0);
+    //   this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+    //   this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+    //   this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+    //   console.log(this.themeTopYs);
+    // });
+
+    //3.请求推荐数据
+    getRecommend().then((res) => {
+      // console.log(res);
+      this.recommends = res.data.list;
+    });
+    //4.给赋值
+    //debounce在哪
+    // this.getThemeTopY = debounce(() => {
+    //   this.themeTopYs.push(0);
+    //   this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+    //   this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+    //   this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+    //   console.log(this.themeTopYs);
+    // }, 100);
+  },
+
+  updated() {
+    this.themeTopYs = [];
+
+    this.themeTopYs.push(0);
+    this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+    this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+    this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+    // console.log(this.themeTopYs);
+  }, //不对,对的是debounce里的,但是那会报错,所以先用这个吧
+  methods: {
+    detailImageLoad() {
+      //detailImageLoad哪来的???
+      this.newRefresh();
+
+      this.getThemeTopY();
+    },
+    titleClick(index) {
+      console.log(index);
+      this.$refs.scroll.scrollTo(0, -this.themeTopYs[index], 300);
+    },
+    backClick() {
+      // console.log("backClick");
+      this.$refs.scroll.scrollTo(0, 0);
+    },
+    contentScroll(position) {
+      // console.log(position);
+      const positionY = -position.y;
+
+      let length = this.themeTopYs.length;
+      for (let i = 0; i < length; i++) {
+        // console.log(i);
+        // if (
+        //   positionY > this.themeTopYs[i] &&
+        //   positionY < this.themeTopYs[i + 1]
+        // ) {
+        //   console.log(i);
+        // }
+        // if (
+        //   this.currentIndex !== i &&
+        //   positionY >= this.themeTopY[i] &&
+        //   positionY < this.themeTopY[i + 1]
+        // ) {
+        //   this.currentIndex = i;
+        //   this.$refs.nav.currentIndex = this.currentIndex;
+        // }
+      }
+      //3.是否显示回到顶部
+      this.isShowBackTop = -position.y > 1000;
+    },
+    addToCart() {
+      // console.log(11);
+      //1.获取购物车需要显示的信息
+      const product = {};
+      product.image = this.topImages[0];
+      product.title = this.goods.title;
+      product.desc = this.goods.desc;
+      product.price = this.goods.realPrice;
+
+      product.iid = this.iid;
+
+      //将商品添加到购物车里
+      this.$store.commit("addCart", product);
+    },
   },
 };
 </script>
 <style scoped>
+.detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+.content {
+  height: calc(100% - 44px);
+  /* -49px有问题? */
+}
 </style>
